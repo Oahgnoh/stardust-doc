230f33cdb3266f261b3a75c92e650bbc38168780
xuhh <xuhh@cheersmind.com>
2024-08-01 18:12:14 +0800

    fix: 修复icon不生效

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { set_sidebar } from "../utils/auto-gen-sidebar.mjs";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/stardust-doc/",
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  head: [["link", { rel: "icon", href: "/stardust-doc/logo.svg" }]],
   title: "Oahgnoh的文档网站",
   description: "A Oahgnoh Site",
   themeConfig: {
